7a7ac2a67cd664a1dba71ca17b98f205526aab15
Randkill <erfan.vh@gmail.com>
2018-11-01 23:17:16 +0330

    initializing Circular Linked List methods

--- src/com/company/CircularLinkedList.java
+++ src/com/company/CircularLinkedList.java
@@ -1,4 +1,47 @@
 package com.company;
 
 public class CircularLinkedList {
+    private Node start, last;
+    CircularLinkedList(){
+        start = null;
+        last = null;
+        //start = last;
+    }
+
+    public void addFirst(int data){
+        Node newNode = new Node();
+        newNode.Data = data;
+        if(start == last && start == null && last == null){
+            last = newNode;
+            //start.next = last;
+            start = newNode;
+
+            last.next = start;
+        }else{
+            last.next = newNode;
+            newNode.next = start;
+            start = newNode;
+        }
+
+    }
+
+    public int deleteFirst(){
+        if(start == null){
+            return -1;
+        }else {
+            Node current = start;
+            start = start.next;
+            last.next = start;
+            return current.Data;
+        }
+    }
+
+    public void printList(){
+        Node currentNode = start;
+        while(currentNode.next != start){
+            System.out.print(currentNode.Data + " ---> ");
+            currentNode = currentNode.next;
+        }
+
+    }
 }
